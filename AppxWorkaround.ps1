669e823aa645c79f414187931f716d1b9b3a878f
gakamor <107076240+gakamor@users.noreply.github.com>
2025-06-16 14:28:45 -0500

    Create AppxWorkaround.ps1

---
+++ AppxWorkaround.ps1
@@ -0,0 +1,31 @@
+﻿<#
+Many Appx PowerShell module cmdlets are currently broken when running 
+them via Enter-PSSession or Invoke-Command on 24H2 or Server 2025 targets. 
+Until Microsoft provides a fix, you may see the following error:
+
+The type initializer for '<Module>' threw an exception.
+    + CategoryInfo          : NotSpecified: (:) [], TypeInitializationException
+    + FullyQualifiedErrorId : System.TypeInitializationException
+
+This script adds runs the Appx command as a job so that it runs locally and
+bypasses the error.
+#>
+
+$job = Start-Job -ScriptBlock {
+    Get-AppxPackage -AllUsers
+}
+
+do {
+    Start-Sleep -Seconds 1
+}
+until ($job.State -eq "Completed")
+
+$output = Receive-Job -Id $job.Id
+foreach ($app in $output) {
+    [PSCustomObject]@{
+        Name    = $app.Name
+        Version = $app.Version
+    }
+}
+
+Remove-Job -Id $job.Id
